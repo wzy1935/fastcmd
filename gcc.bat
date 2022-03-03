96d1f2bece7404ed6abb70824dec94ce66f4785d
wzy1935 <13053260125@163.com>
2022-03-04 00:32:15 +0800

    ?

--- gcc.bat
+++
@@ -1,1 +0,0 @@
-g++ -o resource/local args_forward.cpp
--- install.bat
+++
@@ -1,5 +0,0 @@
-python install -r requirements.txt
-python fastcmd.py init
-fastcmd_load.bat
-python fastcmd.py set fastcmd "python fastcmd.py"
-fastcmd service up
